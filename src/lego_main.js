047f2768f11dd32f149c51cc568b08095e28bae9
Sam Cronin <croninsw@gmail.com>
2019-01-30 13:57:34 -0600

    Initial commit

---
+++ src/lego_main.js
@@ -0,0 +1,25 @@
+// create eventlistener for input save
+document.querySelector(".saveButton").addEventListener("click", () => {
+    const creator = document.querySelector("#lego__creator").value
+    const creation = document.querySelector("#lego__creation").value
+    const color = document.querySelector("#lego__color").selectedIndex
+    const colorValue = document.querySelector("#lego__color")[color].textContent
+    const shape = document.querySelector("#lego__shape").value
+
+    // build data structure
+    let legoToSave = {
+        name: creator,
+        creation_name: creation,
+        creation_color: colorValue,
+        creation_shape: shape
+    }
+    console.log(legoToSave)
+
+    // fetch("http://localhost:3000/posts", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify(legoToSave)
+    // })
+})
